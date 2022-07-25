213d3ff04a8e01897db5b0b16a713c5d381a863c
hanterrian <hanterrian@gmail.com>
2022-07-25 22:48:55 +0300

    Add filter

--- app/Http/Controllers/Front/MainController.php
+++ app/Http/Controllers/Front/MainController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use Artesaos\SEOTools\Facades\SEOTools;
 
 class MainController extends Controller
 {
     public function index()
     {
+        SEOTools::setTitle(__('Home'));
+
         return view('main.index');
     }
 }
--- app/Http/Controllers/Front/ProductController.php
+++ app/Http/Controllers/Front/ProductController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use Artesaos\SEOTools\Facades\SEOTools;
+use Illuminate\Support\Facades\URL;
 
 class ProductController extends Controller
 {
     public function view(Product $product)
     {
+        SEOTools::setTitle($product->title)
+            ->addImages(URL::to($product->thumbnail));
+
         return view('product.view', ['product' => $product]);
     }
 }
