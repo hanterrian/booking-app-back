9685e8268ee27cc7b0c41077946d227b72e6062a
hanterrian <hanterrian@gmail.com>
2022-07-28 08:29:11 +0300

    Switch to bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,8 +30,18 @@
 
     @yield('content')
 
-    <livewire:popup.login-popup-form/>
-    <livewire:popup.register-popup-form/>
+    <div class="modal fade" id="loginPopup" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <livewire:popup.login-popup-form/>
+        </div>
+    </div>
+
+    <div class="modal fade" id="registerPopup" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <livewire:popup.register-popup-form/>
+        </div>
+    </div>
+
 </div>
 
 </body>
--- resources/views/livewire/popup/register-popup-form.blade.php
+++ resources/views/livewire/popup/register-popup-form.blade.php
@@ -1,18 +1,17 @@
-<div class="modal fade" id="registerPopup" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel">{{ __('Register') }}</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                ...
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{ __('Close') }}</button>
-                <button type="button" class="btn btn-primary" wire:click.prevent="register">{{ __('Register') }}</button>
-            </div>
-        </div>
+<div class="modal-content">
+    <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">{{ __('Register') }}</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body">
+        ...
+    </div>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{ __('Close') }}</button>
+        <button type="button" class="btn btn-primary" wire:loading.remove wire:click.prevent="register">{{ __('Register') }}</button>
+        <button type="button" class="btn btn-primary" wire:loading disabled style="display: none">
+            <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+            {{ __('Register') }}
+        </button>
     </div>
 </div>
-
